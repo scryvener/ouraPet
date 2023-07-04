a9b82f957155d263a60fcc8d59e6ffc0d70b798f
scryvener <71054893+scryvener@users.noreply.github.com>
2023-07-03 22:14:53 -0700

    Create Testfunc.js

---
+++ Pages/netlify/functions/Testfunc.js
@@ -0,0 +1,22 @@
+
+
+export const handler = async (event,data) => {
+    
+    
+    //pull in api key
+    //const apiKey=process.env.OpenAIKey
+
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Hello World"})
+    };
+};
+
+
+// exports.handler = async function (event, context) {
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: "Hello World" }),
+//     };
+//   };
