c28793e5fba08145abb0cdbe7fd3c610908800f3
scryvener <71054893+scryvener@users.noreply.github.com>
2023-07-03 22:45:28 -0700

    Update Testfunc.js

--- Pages/netlify/functions/Testfunc.js
+++ Pages/netlify/functions/Testfunc.js
@@ -2,19 +2,46 @@
 
 export const handler = async (event,data) => {
     
-    
-    //pull in api key
-    //const apiKey=process.env.OpenAIKey
+    const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-    console.log("Hello there, how are you")
-    
+    //pull in api key
+    //for testing is pulled from personal access, need to implement Oauth
+    const apiKey=process.env.OURA_PA_KEY
+
+    async function apiCall(path){
+        try {
+            const response = await fetch(path, {
+                method: 'GET',
+                headers: { 
+                    'Authorization': 'Bearer '+apiKey
+                }
+            });
+
+            if (!response.ok) {
+                console.log('Error!')
+                throw new Error(response.statusText);
+            }
+            const jsonData = await response.json();
+
+            return jsonData;
+
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    //dates need to become variables
+    api_call_path='https://api.ouraring.com/v2/usercollection/daily_readiness'
+
+    var apiResponse= await apiCall(api_call_path)
+
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify({message: apiResponse})
     };
-};
 
-
+}
 // exports.handler = async function (event, context) {
 //     return {
 //       statusCode: 200,
