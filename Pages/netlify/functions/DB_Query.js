c71658d1fe007054c8feae37918f1a7e32b5d89d
scryvener <71054893+scryvener@users.noreply.github.com>
2023-07-07 23:26:32 -0700

    Update DB_Query.js

--- Pages/netlify/functions/DB_Query.js
+++ Pages/netlify/functions/DB_Query.js
@@ -13,7 +13,7 @@ const supabase = createClient(SUPA_DB_PATH, SUPA_DB_KEY);
 // Our standard serverless handler function
 exports.handler = async event => {
 
-  let inputData=JSON.parse(event.body)
+  let inputData=event.body
 
   console.log(target_id)
 
